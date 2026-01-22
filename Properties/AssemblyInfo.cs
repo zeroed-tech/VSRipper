485e267bf51b1b491e11cf1759a0a4f9fefa430c
Zeroed <adrian@zeroed.tech>
2026-01-23 10:51:29 +1100

    Fixed metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VSRipper")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCopyright("Developed by Zeroed.tech")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
